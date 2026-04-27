a013588d104be739366161e7be9330f617cc0a1c
anusreem6238-eng <anusreem6238@gmail.com>
2026-04-27 21:52:43 +0530

    Time: 3 ms (30.16%), Space: 12.3 MB (70.41%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        
+        for i in range(len(strs[0])):
+            for s in strs[1:]:
+                # Check boundary or mismatch
+                if i >= len(s) or s[i] != strs[0][i]:
+                    return strs[0][:i]
+        
+        return strs[0]
