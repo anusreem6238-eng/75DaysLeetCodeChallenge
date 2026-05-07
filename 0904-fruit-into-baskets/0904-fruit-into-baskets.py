925c199b4a38cde8f3ff9e7bbf90b7a9c8bfa2f5
anusreem6238-eng <anusreem6238@gmail.com>
2026-05-07 21:20:17 +0530

    Time: 275 ms (33.55%), Space: 17 MB (82.94%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def totalFruit(self, fruits):
+        """
+        :type fruits: List[int]
+        :rtype: int
+        """
+        left = 0
+        count = {}
+        max_fruits = 0
+
+        for right in range(len(fruits)):
+            # Add current fruit
+            fruit = fruits[right]
+            count[fruit] = count.get(fruit, 0) + 1
+
+            # If more than 2 fruit types, shrink window
+            while len(count) > 2:
+                left_fruit = fruits[left]
+                count[left_fruit] -= 1
+
+                if count[left_fruit] == 0:
+                    del count[left_fruit]
+
+                left += 1
+
+            # Update maximum window size
+            max_fruits = max(max_fruits, right - left + 1)
+
+        return max_fruits
