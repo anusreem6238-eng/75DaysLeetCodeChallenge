d8d28a3c1695bbf34e3face42bb22ccfa9041337
anusreem6238-eng <anusreem6238@gmail.com>
2026-05-29 21:18:33 +0530

    Time: 34 ms (66.4%), Space: 12.8 MB (32.07%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,36 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        if not node:
+            return None
+        
+        # Dictionary to store original node -> cloned node
+        clones = {}
+        
+        def dfs(curr):
+            # If already cloned, return it
+            if curr in clones:
+                return clones[curr]
+            
+            # Create clone
+            copy = Node(curr.val)
+            clones[curr] = copy
+            
+            # Clone neighbors
+            for neighbor in curr.neighbors:
+                copy.neighbors.append(dfs(neighbor))
+            
+            return copy
+        
+        return dfs(node)
